b56d8b6a67c6e76dee3b52956af27c3b9f4f9127
Audry Ford <audryford792@gmail.com>
2021-07-26 22:24:59 -0500

    show 5 day outlook on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,50 @@ var cityInputEl = document.querySelector("#city-name");
 var stateInputEl = document.querySelector("#state-code");
 var searchButtonEl = document.querySelector("#search-btn");
 var currentWeatherEl = document.querySelector("#current-weather");
+var savedSearchesEl = document.querySelector(".saved-searches");
 
+var renderSearches = function(searches) {
+	savedSearchesEl.innerHTML = ""
+	for (var i = 0; i < searches.length; i++) {
+		var searchesButtonEl = document.createElement("button");
+		searchesButtonEl.innerHTML = searches[i].city + ", " + searches[i].state;
+		searchesButtonEl.classList.add("btn", "btn-secondary");
+		savedSearchesEl.appendChild(searchesButtonEl);
+	}
+};
 
+if (localStorage.getItem("searches")) {
+	var searches = JSON.parse(localStorage.getItem("searches"));
+	renderSearches(searches);
+} else {
+	var searches = []
+}
 
 var searchSubmitHandler = function () {
 	// get value from input
 	var cityName = cityInputEl.value.trim();
 	var stateName = stateInputEl.value.trim();
 
-	localStorage.setItem("city", cityName);
-	localStorage.setItem("state", stateName);
+	var search = {
+		city: cityName,
+		state: stateName
+	}
+	searches.push(search)
+	renderSearches(searches)
+	localStorage.setItem("searches", JSON.stringify(searches));
+	
 
 	var lookupLocation = cityName + "," + stateName
-
-	console.log(lookupLocation);
 
 	getWeather(lookupLocation);
 
 	//clear old content
 	cityInputEl.value = "";
 	stateInputEl.value = "";
-}
+};
+
+
+
 
 var getWeather = function (lookupLocation) {
 	var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + lookupLocation + ",US&appid=933bc50f9aa095b25c8ea39c0e34c2a8";
@@ -58,75 +81,93 @@ var getWeather = function (lookupLocation) {
 											weatherIcon: forecastData.current.weather[0].icon
 										}
 
+										currentWeatherEl.textContent = "";
 										// dynamically create html elements using this object.
 										var currentCityEl = document.createElement("h1");
-										// add text to created elements
 										currentCityEl.textContent = lookupLocation.toUpperCase();
-										// append created elements
 										currentWeatherEl.appendChild(currentCityEl);
 
-										// create current-weather container
 										var currentDateEl = document.createElement("h2");
-										// add text to created elements
 										currentDateEl.textContent = currentWeather.todaysDate;
-										// append created elements
 										currentWeatherEl.appendChild(currentDateEl);
 
-										// create current-weather container
 										var currentTempEl = document.createElement("div");
-										// add text to created elements
 										currentTempEl.textContent = currentWeather.temp + "℉";
-										// append created elements
 										currentWeatherEl.appendChild(currentTempEl);
 
-										// create current-weather container
 										var currentIconEl = document.createElement("img");
-										// add text to created elements
 										currentIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + currentWeather.weatherIcon + "@2x.png");
-										// append created elements
 										currentWeatherEl.appendChild(currentIconEl);
 
-										// create current-weather container
 										var currentHumidityEl = document.createElement("div");
-										// add text to created elements
 										currentHumidityEl.textContent = "Humidity: " + currentWeather.humidity + "%";
-										// append created elements
 										currentWeatherEl.appendChild(currentHumidityEl);
 
-										// create current-weather container
 										var currentWindEl = document.createElement("div");
-										// add text to created elements
 										currentWindEl.textContent = "Wind Speed: " + currentWeather.windSpeed + "MPH";
-										// append created elements
 										currentWeatherEl.appendChild(currentWindEl);
 
-										// create current-weather container
 										var currentUVEl = document.createElement("div");
-										// add text to created elements
 										currentUVEl.textContent = "UV Index: " + currentWeather.UVindex;
-										// append created elements
 										currentWeatherEl.appendChild(currentUVEl);
 
-										
 
 
 
 
+										var fiveDayEl = document.querySelector("#five-day")
 										console.log(currentWeather);
-
+										fiveDayEl.innerHTML = ""
 										for (var i = 0; i < 5; i++) {
 											var fiveDayForecast = {
 												forecastDate: moment.unix(forecastData.daily[i].dt).format("M/D/YYYY"),
-												forecastTemp: forecastData.daily[i].temp,
+												forecastTemp: forecastData.daily[i].temp.day,
 												forecastHumidity: forecastData.daily[i].humidity,
 												forecastWind: forecastData.daily[i].wind_speed,
 												forecastIcon: forecastData.daily[i].weather[0].icon
 											}
+											console.log()
 											console.log(fiveDayForecast);
+
+
+											// using a for loop(?)(or in this^ for loop) dynamically create html elements using these objects
+											
+											
+											var cardEl = document.createElement("div");
+											cardEl.classList.add("card");
+
+											var futureTitleEl = document.createElement("h5");
+											futureTitleEl.classList.add("card-title");
+
+											var futureBodyEl = document.createElement("p");
+											futureBodyEl.classList.add("card-text");
+
+											var forecastDateEl = document.createElement("div");
+											forecastDateEl.textContent = fiveDayForecast.forecastDate;
+											futureTitleEl.appendChild(forecastDateEl);
+
+											var futureTempEl = document.createElement("div");
+											futureTempEl.textContent = fiveDayForecast.forecastTemp + "℉";
+											futureTitleEl.appendChild(futureTempEl);
+
+
+											var futureIconEl = document.createElement("img");
+											futureIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + fiveDayForecast.forecastIcon + "@2x.png");
+											futureBodyEl.appendChild(futureIconEl);
+
+											var futureHumidityEl = document.createElement("div");
+											futureHumidityEl.textContent = "Humidity: " + fiveDayForecast.forecastHumidity + "%";
+											futureBodyEl.appendChild(futureHumidityEl);
+
+											var futureWindEl = document.createElement("div");
+											futureWindEl.textContent = "Wind Speed: " + fiveDayForecast.forecastWind + "MPH";
+											futureBodyEl.appendChild(futureWindEl);
+
+											cardEl.appendChild(futureTitleEl);
+											cardEl.appendChild(futureBodyEl);
+											fiveDayEl.appendChild(cardEl);
+
 										}
-
-										// using a for loop(?)(or in this^ for loop) dynamically create html elements using these objects
-
 
 									})
 							})
@@ -142,5 +183,8 @@ var getWeather = function (lookupLocation) {
 // event listeners
 searchButtonEl.addEventListener("click", searchSubmitHandler);
 
-
+savedSearchesEl.addEventListener("click", function(event) {
+	console.log(event.target);
+	getWeather(event.target.innerHTML);
+})
 
